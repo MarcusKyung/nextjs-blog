4b87244e8442fb9b0b4e0ed19392af46e0b572c6
MarcusKyung <Kyungmj@gmail.com>
2023-08-18 14:43:18 -0700

    add css modules

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
-
+import styles from '../../styles/first-post.module.css';
 
 
 export default function FirstPost() {
@@ -17,7 +17,7 @@ export default function FirstPost() {
           console.log(`script loaded correctly, window.FB has been populated`)
         }
       />
-      <h1>First Post</h1>
+      <h1 className={styles.postTitle}>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
